01eff7034f0181260acc9fb81827cc93d684b8ca
Sahil-195 <119130858+Sahil-195@users.noreply.github.com>
2024-05-15 09:03:31 +0000

    Const let & var

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -10,5 +10,9 @@ accountEmail = "sp@sp.in"
 accountPassword = "3432432"
 accountCity = "Bangalore"
 
+/*
+    Prefer not to use var because of issue in block scope and functional scope
+*/
+
 console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
