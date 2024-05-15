a1f910dd38d605a3752597b9ec580b0e94de3b7c
Sahil-195 <119130858+Sahil-195@users.noreply.github.com>
2024-05-15 09:02:13 +0000

    Const let & var

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "sahilpajwani@google.com"
+var accountPassword = "322332"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2
+
+accountEmail = "sp@sp.in"
+accountPassword = "3432432"
+accountCity = "Bangalore"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
