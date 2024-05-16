abb79fa76ce2c558128de190b631762ec48af122
Sahil-195 <119130858+Sahil-195@users.noreply.github.com>
2024-05-16 09:36:17 +0000

    maths and num

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,37 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(3.2));
+console.log(Math.round(3.6));
+console.log(Math.ceil(3.2));
+console.log(Math.floor(3.9));
+
+console.log(Math.min(3.9,45,435,38,34));
+console.log(Math.max(3.9,45,435,3,34));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random()*10)) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
