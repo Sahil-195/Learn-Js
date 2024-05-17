b96f8cbbc0e0228e8e21b90708bf4208e9aed175
Sahil-195 <119130858+Sahil-195@users.noreply.github.com>
2024-05-17 12:51:02 +0000

    Json talk

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -5,7 +5,7 @@ tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
 const regularUser = {
     email: "some@google.com",
@@ -16,7 +16,7 @@ const regularUser = {
         }
     }
 }
-console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname);
 
 const obj1 = { 1: 'a', 2: 'b' }
 const obj2 = { 3: 'a', 4: 'b' }
@@ -27,7 +27,7 @@ const obj4 = { 5: 'a', 6: 'b' }
 
 const obj3 = { ...obj1, ...obj2 };
 
-console.log(obj3);
+// console.log(obj3);
 
 const users = [
     {
@@ -44,13 +44,31 @@ const users = [
     },
 ]
 
-console.log(users[1].email);
+// console.log(users[1].email);
 
-console.log(Object.keys(tinderUser))
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
 
-console.log(tinderUser.hasOwnProperty("isLoggedIn"))
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"))
 
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
 
 
